946535e0556b04ce44e7a2e7a7df70ac2e250d99
samer <sameradel789@gmail.com>
2023-08-18 22:49:25 +0300

    transition to homePage

--- lib/Features/Splash/presentation/views/widgets/splach_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splach_view_body.dart
@@ -1,6 +1,11 @@
 import 'package:bookly/Features/Splash/presentation/views/widgets/sliding_logo.dart';
+import 'package:bookly/const.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import '../../../../home/presentation/views/home_view.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,12 +21,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    slidinganimation =
-        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHomePage();
   }
 
   @override
@@ -45,4 +46,20 @@ class _SplashViewBodyState extends State<SplashViewBody>
           SlidingLogo(slidinganimation: slidinganimation)
         ]);
   }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidinganimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHomePage() {
+    Future.delayed(Duration(seconds: 3), () {
+      Get.to(HomeView(),
+          transition: Transition.fade, duration: Ktransitionduration);
+    });
+  } 
 }
--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const KprimaryColor = Color(0xff100B20);
+const Ktransitionduration = Duration(milliseconds: 250);
