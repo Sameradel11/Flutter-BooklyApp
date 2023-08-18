012f92c855009d0e0e95b8213b3f0813d919f697
samer <sameradel789@gmail.com>
2023-08-18 20:59:21 +0300

    build animation in splashscreen

---
+++ lib/Features/Splash/presentation/views/widgets/sliding_logo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class SlidingLogo extends StatelessWidget {
+  const SlidingLogo({
+    super.key,
+    required this.slidinganimation,
+  });
+
+  final Animation<Offset> slidinganimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidinganimation,
+      builder: (BuildContext context, Widget? child) => SlideTransition(
+          position: slidinganimation,
+          child: Image.asset(AssetsData.logo)),
+    );
+  }
+}
--- lib/Features/Splash/presentation/views/widgets/splach_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splach_view_body.dart
@@ -1,8 +1,29 @@
+import 'package:bookly/Features/Splash/presentation/views/widgets/sliding_logo.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinganimation;
+  @override
+  void initState()  {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidinganimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +37,7 @@ class SplashViewBody extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 8),
-          Image.asset(AssetsData.logo)
+          SlidingLogo(slidinganimation: slidinganimation)
         ]);
   }
 }
