09ebe3f056710529bb1d86fce11a05b67f273f6a
samer <sameradel789@gmail.com>
2023-08-18 17:38:07 +0300

    Build Splash view

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'Features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView();
+  }
+}
