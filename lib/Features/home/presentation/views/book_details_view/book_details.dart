d48646a7ed40e94e29a2133d516eb64fc635e582
samer <sameradel789@gmail.com>
2023-08-21 01:47:11 +0300

    create book details page

--- lib/Features/home/presentation/views/book_details_view/book_details.dart
+++ lib/Features/home/presentation/views/book_details_view/book_details.dart
@@ -1,11 +1,35 @@
+import 'package:bookly/Features/home/presentation/views/homeView/widgets/custom_app_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class BookDetails extends StatelessWidget {
+  const BookDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: Row(children: [
+            const Icon(Icons.cancel),
+            const Spacer(
+              flex: 8,
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  size: 28,
+                )),
+            const Spacer(
+              flex: 1,
+            ),
+          ]),
+        )
+      ],
+    );
   }
 }
--- lib/core/utils/app_route.dart
+++ lib/core/utils/app_route.dart
@@ -2,10 +2,15 @@ import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly/Features/home/presentation/views/homeView/home_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../Features/home/presentation/views/book_details_view/book_details.dart';
+
 abstract class AppRouter {
   static const KHomeView = "/homeview";
+  static const KDetailsView = '/details';
   static final router = GoRouter(routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashView()),
     GoRoute(path: '/homeview', builder: (context, state) => const HomeView()),
+    GoRoute(
+        path: "/details", builder: ((context, state) => const BookDetails()))
   ]);
 }
