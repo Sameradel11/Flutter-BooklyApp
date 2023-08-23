6c687706b58de9daf731f593ebe4d1a223d89100
samer <sameradel789@gmail.com>
2023-08-23 23:11:41 +0300

    finish UI

---
+++ lib/Features/search/presentation/views/widgets/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/Features/search/presentation/views/widgets/search_view_body.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          hintText: "Search",
+          suffixIcon: const Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          enabledBorder: customBorder(),
+          focusedBorder: customBorder()),
+    );
+  }
+}
+
+OutlineInputBorder customBorder() {
+  return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(9),
+      borderSide: const BorderSide(color: Colors.white));
+}
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,40 @@
+import 'package:bookly/Features/home/presentation/views/homeView/widgets/best_seller_listview.dart';
+import 'package:bookly/core/utils/style.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            const CustomTextField(),
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25, bottom: 10),
+              child: Text(
+                "Result",
+                style: Style.textstyle20
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.w900),
+              ),
+            ),
+            const BestSellerListView()
+          ],
+        ),
+      ),
+    );
   }
 }
