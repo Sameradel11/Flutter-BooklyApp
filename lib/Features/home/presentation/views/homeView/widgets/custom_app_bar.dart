47d3f62a36b6001c87632ae6b4114e772f7c77ac
samer <sameradel789@gmail.com>
2023-08-23 22:14:05 +0300

    create search view

--- lib/Features/home/presentation/views/homeView/widgets/custom_app_bar.dart
+++ lib/Features/home/presentation/views/homeView/widgets/custom_app_bar.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utils/app_route.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../../core/utils/assets.dart';
 
@@ -13,13 +15,16 @@ class CustomAppBar extends StatelessWidget {
       padding: const EdgeInsets.only(left: 30),
       child: Row(children: [
         Image.asset(
-          AssetsData.logo, height: 16,
+          AssetsData.logo,
+          height: 16,
         ),
         const Spacer(
           flex: 8,
         ),
         IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.KSearchView);
+            },
             icon: const Icon(
               Icons.search,
               size: 28,
--- lib/core/utils/app_route.dart
+++ lib/core/utils/app_route.dart
@@ -2,6 +2,7 @@
 
 import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly/Features/home/presentation/views/homeView/home_view.dart';
+import 'package:bookly/Features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../Features/home/presentation/views/book_details_view/book_details_view.dart';
@@ -9,11 +10,14 @@ import '../../Features/home/presentation/views/book_details_view/book_details_vi
 abstract class AppRouter {
   static const KHomeView = "/homeview";
   static const KDetailsView = '/details';
+  static const KSearchView = '/searchview';
+
   static final router = GoRouter(routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashView()),
-    GoRoute(path: '/homeview', builder: (context, state) => const HomeView()),
+    GoRoute(path: KHomeView, builder: (context, state) => const HomeView()),
     GoRoute(
-        path: "/details",
-        builder: ((context, state) => const BookDetailsView()))
+        path: KDetailsView,
+        builder: ((context, state) => const BookDetailsView())),
+    GoRoute(path: KSearchView, builder: (context, state) => const SearchView()),
   ]);
 }
