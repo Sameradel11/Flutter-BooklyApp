9d8bbe644545759310fc3b103d5ea54acaa85309
samer <sameradel789@gmail.com>
2023-08-24 18:50:34 +0300

    createa api class

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final String baseurl = "https://www.googleapis.com/books/v1/";
+  final Dio dio;
+
+  ApiServices({required this.dio});
+  Future<Map<String, dynamic>> get({required String endpoints}) async {
+    var response = await dio.get('$baseurl$endpoints');
+    print("Response is $response");
+    print("but the data only in that response is $response.data");
+    return response.data;
+  }
+}
